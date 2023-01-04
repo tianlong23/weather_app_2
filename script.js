51ab19f0e3273112154c39c10612836312dc3224
tianlong23 <longch23@gmail.com>
2023-01-05 06:25:46 +0800

    create index, style, and script files. Basic outline of HTML and project outline in script file.

---
+++ script.js
@@ -0,0 +1,16 @@
+//STEPS
+//Find Free API for the Weather, see what data is available
+//find free API for flags based on country input
+//create HTML with spaces for each of the pieces of information I want
+//create CSS to look nice
+//Create JS to pull relevant into in
+//first setup API as an object, make sure data is coming to the page
+//then take data from the API, put into variables or consts, and display in the app
+//make everything look nice, add any other cool features available such as a changing background
+
+
+//link https://api.weatherbit.io/v2.0/current?&city=shanghai&key=d53492af289a4192aa1756cb771b4f09&include=minutely
+//API key: d53492af289a4192aa1756cb771b4f09
+
+//data desired: country code, which then pulls the flag, temp, icon, wind speed, description, AQI
+//example icon link: https://www.weatherbit.io/static/img/icons/t01d.png
