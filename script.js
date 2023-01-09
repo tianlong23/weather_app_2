96b11a383d2d2521b518cc4cb65abb92c0e7f855
tianlong23 <longch23@gmail.com>
2023-01-10 06:37:32 +0800

    Create basic working API

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //STEPS
-//Find Free API for the Weather, see what data is available
-//find free API for flags based on country input
-//create HTML with spaces for each of the pieces of information I want
-//create CSS to look nice
+//Find Free API for the Weather, see what data is available - DONE
+//find free API for flags based on country input - DONE
+//create HTML with spaces for each of the pieces of information I want - DONE
+//create CSS to look nice - DONE
 //Create JS to pull relevant into in
 //first setup API as an object, make sure data is coming to the page
 //then take data from the API, put into variables or consts, and display in the app
@@ -14,3 +14,24 @@
 
 //data desired: country code, which then pulls the flag, temp, icon, wind speed, description, AQI
 //example icon link: https://www.weatherbit.io/static/img/icons/t01d.png
+fetch('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0q1kzenolJKT7EMMi51UBJ0M4r1VTHyF&q=shanghai')
+.then(res => res.json())
+.then((data) => {
+    console.log(data)
+    const {EnglishName, Country: {ID}} = data[0]
+    const {Key} = data[0]
+    console.log(ID)
+    console.log(EnglishName)
+    console.log(Key)
+        fetch('http://dataservice.accuweather.com/currentconditions/v1/' + Key + '?apikey=0q1kzenolJKT7EMMi51UBJ0M4r1VTHyF')
+        .then(info => info.json())
+        .then((info) => {
+            console.log(info)
+            const { Temperature: {Metric: { Value}}} = info[0]
+            const {WeatherText, WeatherIcon} = info[0]
+            console.log(Value, WeatherText, WeatherIcon)
+        })
+})
+
+//continue getting data, see how to pull into the fields in the DOM. 
+//Then see how to pull based on search request in the HTML
