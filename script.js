9a0ad03cbba003fbb92a891744a500f6489a37e7
tianlong23 <longch23@gmail.com>
2023-01-13 19:23:07 +0800

    final version uploaded with all working features

--- script.js
+++ script.js
@@ -1,37 +1,69 @@
-//STEPS
-//Find Free API for the Weather, see what data is available - DONE
-//find free API for flags based on country input - DONE
-//create HTML with spaces for each of the pieces of information I want - DONE
-//create CSS to look nice - DONE
-//Create JS to pull relevant into in
-//first setup API as an object, make sure data is coming to the page
-//then take data from the API, put into variables or consts, and display in the app
-//make everything look nice, add any other cool features available such as a changing background
 
+/*Function to fetch the city API, get the city ID, and pass this to another API to get the conditions, then get the attributes and set them to constants*/
+function getData (cityName) {
+    fetch('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0q1kzenolJKT7EMMi51UBJ0M4r1VTHyF&q=' + cityName + '')
+    .then(res => res.json())
+    .then((data) => {
+        console.log('data: '+ data)
+        const {EnglishName, Country: {ID}} = data[0]
+        console.log('english name: '+ EnglishName);
+        const {Key} = data[0]
+            fetch('http://dataservice.accuweather.com/currentconditions/v1/' + Key + '?apikey=0q1kzenolJKT7EMMi51UBJ0M4r1VTHyF')
+            .then(info => info.json())
+            .then((info) => {
+                const { Temperature: {Metric: { Value}}} = info[0];
+                const {WeatherText, WeatherIcon} = info[0];
+                updateData(EnglishName, Value, WeatherIcon, WeatherText, ID);
+            })
+    })
+    .catch(error => {
+        console.log('here')
+        document.getElementsByClassName('dataContainer')[0].setAttribute('id', 'tryAgain');
+        console.error('catch error', error)
+    })
+}
 
-//link https://api.weatherbit.io/v2.0/current?&city=shanghai&key=d53492af289a4192aa1756cb771b4f09&include=minutely
-//API key: d53492af289a4192aa1756cb771b4f09
+/*When the page loads, runs a first search with shanghai as a city choice. Then runs the API function. */
 
-//data desired: country code, which then pulls the flag, temp, icon, wind speed, description, AQI
-//example icon link: https://www.weatherbit.io/static/img/icons/t01d.png
-fetch('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0q1kzenolJKT7EMMi51UBJ0M4r1VTHyF&q=shanghai')
-.then(res => res.json())
-.then((data) => {
-    console.log(data)
-    const {EnglishName, Country: {ID}} = data[0]
-    const {Key} = data[0]
-    console.log(ID)
-    console.log(EnglishName)
-    console.log(Key)
-        fetch('http://dataservice.accuweather.com/currentconditions/v1/' + Key + '?apikey=0q1kzenolJKT7EMMi51UBJ0M4r1VTHyF')
-        .then(info => info.json())
-        .then((info) => {
-            console.log(info)
-            const { Temperature: {Metric: { Value}}} = info[0]
-            const {WeatherText, WeatherIcon} = info[0]
-            console.log(Value, WeatherText, WeatherIcon)
+/*This function also sets event listeners for clicking on the search icon and clicking the enter key for a new search*/
+window.onload=function(){
+    cityName = 'shanghai';
+    getData(cityName)
+    const findCity = document.getElementsByClassName("searchButton")[0];
+    findCity.addEventListener('click', function() {        
+        document.getElementsByClassName('dataContainer')[0].setAttribute('id', 'hideData');
+        var cityLocation = document.getElementsByClassName("search-bar")[0];
+        var cityName = cityLocation.value;
+        getData (cityName);
+    
+    })
+    const searchEnter = document.getElementsByClassName("search-bar")[0];
+    searchEnter.addEventListener('keypress', function(e) {
+        if(e.key === 'Enter') {
+            document.getElementsByClassName('dataContainer')[0].setAttribute('id', 'hideData');
+            var cityLocation = document.getElementsByClassName("search-bar")[0];
+            var cityName = cityLocation.value;
+            getData (cityName);
+        }
         })
-})
+  }
 
-//continue getting data, see how to pull into the fields in the DOM. 
-//Then see how to pull based on search request in the HTML
+  /* This function updates the DOM elements with the attributes pulled from the API*/
+function updateData (EnglishName, Value, WeatherIcon, WeatherText, ID) {
+    const name = document.getElementsByClassName('name')[0];
+    name.innerText = 'Weather in ' + EnglishName;
+    const temp = document.getElementsByClassName('temp')[0];
+    temp.innerText = 'Temperature: ' + Value + '° C';
+    const description = document.getElementsByClassName('description')[0];
+    description.innerText = WeatherText
+    /* This if statement adds a '0' to the icon if the icon number is less than 10, as it is missing an intial '0'*/
+    if (WeatherIcon < 10) {
+    document.getElementsByClassName('icon')[0].src='https://developer.accuweather.com/sites/default/files/0' + WeatherIcon + '-s.png'
+    } else {
+    document.getElementsByClassName('icon')[0].src='https://developer.accuweather.com/sites/default/files/' + WeatherIcon + '-s.png'
+    }
+    document.getElementsByClassName('flag')[0].src='https://flagsapi.com/' + ID + '/flat/64.png';
+    document.body.style.backgroundImage = 'url("https://source.unsplash.com/1600x900/?' + EnglishName + '")';
+    setTimeout(document.getElementsByClassName('dataContainer')[0].removeAttribute('id', 'hideData'), 1000);
+
+}
